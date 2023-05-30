e8d203f647d8dc416a9848d792c9b01e41cf1481
Lawrence-Tang <tangzongsheng@gmail.com>
2023-05-30 10:20:25 +0800

    pre-prepared userdata.img

--- tools/update_prebuilt.sh
+++ tools/update_prebuilt.sh
@@ -9,14 +9,7 @@ set -eu
 cp -f $2/bl1-mmcboot.bin $1/
 cp -f $2/loader-mmc.img $1/
 cp -f $2/bl_mon.img $1/
-
-if [ ! -f $1/userdata.img ]; then
-	USERDATA_SIZE=209715200
-	echo "Generating empty userdata.img (size:${USERDATA_SIZE})"
-	TMPDIR=`mktemp -d`
-	${PWD}/tools/make_ext4fs -s -l ${USERDATA_SIZE} -a root -L userdata $1/userdata.img ${TMPDIR}
-	rm -rf ${TMPDIR}
-fi
+[ ! -f $1/userdata.img ] && cp -f $2/userdata.img $1/
 
 if [ ! -f $1/env.conf ]; then
     cp -f $2/env.conf $1/
