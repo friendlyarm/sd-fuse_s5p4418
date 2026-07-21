friendlyarm/sd-fuse_rk3588

Migrate image downloads to CDN and switch verify from md5 to sha256

test/test-big-rootfs-img.sh
@@ -1,8 +1,13 @@
 #!/bin/bash
 set -eu
 
-HTTP_SERVER=112.124.9.243
-
+# CDN base URL: local MinIO when .use-local-r2 exists at repo root,
+# else production Cloudflare R2. All images-for-eflasher downloads go here.
+if [ -f "$(dirname "$(readlink -f "$0")")/../.use-local-r2" ]; then
+    CDN_URL=http://cdn.local/friendlyelec-cdn/os-images/rk3588/images
+else
+    CDN_URL=https://cdn.friendlyelec.com/os-images/rk3588/images
+fi
 # hack for me
 [ -f /etc/friendlyarm ] && source /etc/friendlyarm $(basename $(builtin cd ..; pwd))
 
@@ -13,9 +18,9 @@ sudo rm -rf tmp/*
 cd tmp
 git clone ../../.git sd-fuse_rk3588
 cd sd-fuse_rk3588
-wget --no-proxy http://${HTTP_SERVER}/dvdfiles/RK3588/old/kernel-5.10.y/images-for-eflasher/debian-bullseye-desktop-arm64-images.tgz
+wget --no-proxy ${CDN_URL}/debian-bullseye-desktop-arm64-images.tgz
 tar xzf debian-bullseye-desktop-arm64-images.tgz
-wget --no-proxy http://${HTTP_SERVER}/dvdfiles/RK3588/old/kernel-5.10.y/images-for-eflasher/emmc-flasher-images.tgz
+wget --no-proxy ${CDN_URL}/emmc-flasher-images.tgz
 tar xzf emmc-flasher-images.tgz
 
 # make big file

test/test-update-uboot.sh
@@ -1,7 +1,13 @@
 #!/bin/bash
 set -eux
 
-HTTP_SERVER=112.124.9.243
+# CDN base URL: local MinIO when .use-local-r2 exists at repo root,
+# else production Cloudflare R2. All images-for-eflasher downloads go here.
+if [ -f "$(dirname "$(readlink -f "$0")")/../.use-local-r2" ]; then
+    CDN_URL=http://cdn.local/friendlyelec-cdn/os-images/rk3588/images
+else
+    CDN_URL=https://cdn.friendlyelec.com/os-images/rk3588/images
+fi
 UBOOT_REPO=https://github.com/friendlyarm/uboot-rockchip
 UBOOT_BRANCH=nanopi5-v2017.09
 
@@ -18,7 +24,7 @@ cd sd-fuse_rk3588
 if [ -f ../../ubuntu-jammy-minimal-arm64-images.tgz ]; then
 	tar xvzf ../../ubuntu-jammy-minimal-arm64-images.tgz
 else
-	wget --no-proxy http://${HTTP_SERVER}/dvdfiles/RK3588/old/kernel-5.10.y/images-for-eflasher/ubuntu-jammy-minimal-arm64-images.tgz
+	wget --no-proxy ${CDN_URL}/ubuntu-jammy-minimal-arm64-images.tgz
     tar xvzf ubuntu-jammy-minimal-arm64-images.tgz
 fi
 

friendlyarm/sd-fuse_s5p4418

test/test-big-rootfs-img.sh
@@ -1,8 +1,11 @@
 #!/bin/bash
 set -eux
 
-HTTP_SERVER=112.124.9.243
-
+if [ -f "$(dirname "$(readlink -f "$0")")/../.use-local-r2" ]; then
+    CDN_URL=http://cdn.local/friendlyelec-cdn/os-images/s5p4418/images
+else
+    CDN_URL=https://downloads.friendlyelec.com/os-images/s5p4418/images
+fi
 # hack for me
 [ -f /etc/friendlyarm ] && source /etc/friendlyarm $(basename $(builtin cd ..; pwd))
 
@@ -13,9 +16,9 @@ sudo rm -rf tmp/*
 cd tmp
 git clone ../../.git sd-fuse_s5p4418
 cd sd-fuse_s5p4418
-wget --no-proxy http://${HTTP_SERVER}/dvdfiles/s5p4418/images-for-eflasher/friendlycore-images.tgz
+wget ${CDN_URL}/friendlycore-images.tgz
 tar xzf friendlycore-images.tgz
-wget --no-proxy http://${HTTP_SERVER}/dvdfiles/s5p4418/images-for-eflasher/emmc-flasher-images.tgz
+wget ${CDN_URL}/emmc-flasher-images.tgz
 tar xzf emmc-flasher-images.tgz
 
 # make big file

test/test-update-uboot.sh
@@ -1,8 +1,11 @@
 #!/bin/bash
 set -eux
 
-HTTP_SERVER=112.124.9.243
-
+if [ -f "$(dirname "$(readlink -f "$0")")/../.use-local-r2" ]; then
+    CDN_URL=http://cdn.local/friendlyelec-cdn/os-images/s5p4418/images
+else
+    CDN_URL=https://downloads.friendlyelec.com/os-images/s5p4418/images
+fi
 # hack for me
 [ -f /etc/friendlyarm ] && source /etc/friendlyarm $(basename $(builtin cd ..; pwd))
 
@@ -13,7 +16,7 @@ sudo rm -rf tmp/*
 cd tmp
 git clone ../../.git sd-fuse_s5p4418
 cd sd-fuse_s5p4418
-wget --no-proxy http://${HTTP_SERVER}/dvdfiles/S5P4418/images-for-eflasher/friendlycore-images.tgz
+wget ${CDN_URL}/friendlycore-images.tgz
 tar xzf friendlycore-images.tgz
 
 git clone https://github.com/friendlyarm/u-boot --depth 1 -b nanopi2-v2016.01 uboot-s5p4418